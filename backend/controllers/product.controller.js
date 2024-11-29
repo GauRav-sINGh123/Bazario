993e7c91309808c746c0bbdad072bf7723766f62
Gaurav Singh <gksingh947@gmail.com>
2024-11-29 22:33:27 +0530

    Create Product Controller Created

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import Product from "../models/product.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import  cloudinary from "../utils/cloudinary.js";
 import { redis } from "../db/redis.js";
 
 export const getAllProducts = asyncHandler(async (req, res) => {
@@ -29,4 +30,35 @@ export const featuredProducts = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, featuredProducts));
 });
 
- 
+ export const createProduct = asyncHandler(async (req, res) => {
+    const { name, description, price, image, category,stock } = req.body;
+
+    if (
+      [name, description, price, image, category,stock].some((field) => field?.trim() === "")
+    ) {
+      throw new ApiError(400, "All fields are required");
+    }
+    
+    if (isNaN(price) || price <= 0) {
+        throw new ApiError(400, "Price must be a valid positive number");
+    }
+
+    let cloudinaryResponse=null;
+
+    if (image) {
+        try {
+          cloudinaryResponse = await cloudinary.uploader.upload(image, { folder: "products" });
+        } catch (err) {
+          throw new ApiError(500, "Failed to upload image to Cloudinary");
+        }
+      }
+    const product =await Product.create({
+        name,
+        description,
+        price,
+        image:cloudinaryResponse?.secure_url ? cloudinaryResponse?.secure_url:"",
+        category,
+        stock
+    })
+      return res.status(201).json(new ApiResponse(201, product));
+ })
--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -27,6 +27,10 @@ const productSchema = new Schema({
     isFeatured:{
         type:Boolean,
         default:false
+    },
+    stock:{
+        type:Number,
+        default:0
     }
 },{timestamps:true});
 
