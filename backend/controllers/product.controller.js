243818b9059bb929adff28831208be4636c76f8c
Gaurav Singh <gksingh947@gmail.com>
2024-11-30 22:37:55 +0530

    Get Recommended Product Method Created

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -82,3 +82,27 @@ export const featuredProducts = asyncHandler(async (req, res) => {
     await Product.findByIdAndDelete(id);
     return res.status(200).json(new ApiResponse(200, "Product deleted successfully"));
   });
+
+  export const getReccomendedProducts = asyncHandler(async (req, res) => {
+    try {
+      const products = await Product.aggregate([
+        {
+          $sample: { size: 4 },
+        },
+        {
+          $project: {
+            _id: 1,
+            name: 1,
+            description: 1,
+            image: 1,
+            price: 1,
+          },
+        },
+      ]);
+  
+      res.json(products);
+    } catch (error) {
+      console.log("Error in getRecommendedProducts controller", error.message);
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
+  });
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
 const router = express.Router();
 import { isAdmin, isProtected } from "../middlewares/auth.middleware.js";
-import { getAllProducts,featuredProducts,createProduct,deleteProduct } from "../controllers/product.controller.js";
+import { getAllProducts,featuredProducts,createProduct,deleteProduct,getReccomendedProducts } from "../controllers/product.controller.js";
 
 router.route("/").get(isProtected,isAdmin,getAllProducts);
 
-router.route("/featured-products").post(featuredProducts);
+router.route("/featured-products").get(featuredProducts);
 
 router.route("/create-product").post(isProtected,isAdmin,createProduct);
 
 router.route('/:id').delete(isProtected,isAdmin,deleteProduct);
+
+router.route('/reccomendation').get(isProtected,getReccomendedProducts);
 
 export default router
