bc82f895263d17f0e17632f1b46db8561751e8a4
Gaurav Singh <gksingh947@gmail.com>
2024-12-03 12:23:26 +0530

    Update Product Quantity Method Created

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -3,9 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
  
 
-
-
-
+// Adds the product into the cart
 export const addToCart=asyncHandler(async(req,res)=>{
     const {productId}=req.body;
     const user=req.user;
@@ -21,6 +19,8 @@ export const addToCart=asyncHandler(async(req,res)=>{
     return res.status(200).json(new ApiResponse(200,user.cartItems))
 })
 
+//Delete functionallity for removing the products from the cart 
+
 export const deleteAllProductsFromCart=asyncHandler(async(req,res)=>{
     const {productId}=req.body;
     const user=req.user;
@@ -31,4 +31,26 @@ export const deleteAllProductsFromCart=asyncHandler(async(req,res)=>{
     }
     await user.save();
     return res.status(200).json(new ApiResponse(200,user.cartItems))
+})
+
+// Update Method for updating the quantity of the products
+
+export const updateProductQuantity=asyncHandler(async(req,res)=>{
+    const {id:productId}=req.params;
+    const {quantity}=req.body;
+    const user=req.user;
+    const existingItem=user.cartItems.find((item)=>item.id===productId)
+    if(existingItem){
+        if(quantity===0){
+            user.cartItems=user.cartItems.filter((item)=>item.id!==productId)
+            await user.save()
+            return res.status(200).json(user.cartItems);
+        }
+        existingItem.quantity=quantity;
+        await user.save();
+        res.status(200).json(new ApiResponse(200,"Product Quantity Updated"))
+        
+    }else{
+        throw new ApiError(400,"Product Not Found")
+    }
 })
