00e0ab4f8762c51facba35917a115e49f4447d2a
Gaurav Singh <gksingh947@gmail.com>
2024-12-30 21:21:11 +0530

    Signup Schema Fixed

--- frontend/src/validation/Validation.ts
+++ frontend/src/validation/Validation.ts
@@ -1,4 +1,6 @@
 import {z} from "zod"
+ 
+ 
 
 export const SignInSchema= z.object({
     email: z.string().email({ message: "Invalid email address" }),
@@ -13,4 +15,6 @@ export const SignInSchema= z.object({
      name: z.string().min(1, { message: "Name is required" }),
      password: z
        .string().min(8, { message: "Password must be at least 8 characters long" }),
+      avatar:z.string().min(1, { message: "Avatar is required" }),
+       
   })
